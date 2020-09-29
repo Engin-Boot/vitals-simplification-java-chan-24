892bee3175de725f8d4df68ac6c89c08fa96ed4d
Chandrashekar Arun Naik <canaik24@gmail.com>
2020-09-29 11:09:51 +0530

    Update Main.java
    Added new tests

--- vitals/Main.java
+++ vitals/Main.java
@@ -1,20 +1,24 @@
 package vitals;
 
 public class Main {
-    static boolean vitalsAreOk(float bpm, float spo2, float respRate) {
-        if(bpm < 70 || bpm > 150) {
-            return false;
-        } else if(spo2 < 90) {
-            return false;
-        } else if(respRate < 30 || respRate > 95) {
-            return false;
-        }
-        return true;
-    }
 
     public static void main(String[] args) {
-        assert(vitalsAreOk(80, 97, 40) == true);
-        assert(vitalsAreOk(60, 98, 40) == false);
-        System.out.println("Some more tests needed");
+    	VitalsCheck check = new VitalsCheck();
+    	
+        assert(check.areVitalsOk(100, 100, 80)) == true; //Vitals Normal
+        assert(check.areVitalsOk(160, 95, 70))== false; // High BP
+        assert(check.areVitalsOk(60, 95, 70))== false; // Low BP
+        assert(check.areVitalsOk(100, 80, 80))== false; // Low spo2
+        assert(check.areVitalsOk(100, 95, 20))== false; //low respRate
+        assert(check.areVitalsOk(100, 95, 115))== false; // high respRate
+        assert(check.areVitalsOk(160, 80, 70))== false; // BP high, low spo2
+        assert(check.areVitalsOk(100, 80, 20))== false; // low spo2, low repRate
+        assert(check.areVitalsOk(100, 55, 110))== false; //spo2 low, respRate high
+        assert(check.areVitalsOk(160, 100, 20))== false; //BP high, respRate low
+        assert(check.areVitalsOk(60, 100, 100))== false; //BP low, respRate high
+        assert(check.areVitalsOk(60, 80, 20))== false; // BP low, spo2 low, respRate low
+        assert(check.areVitalsOk(160, 80, 100))== false; //BP high, spo2 low, respRate high
+        System.out.println("Tests completed");
     }
+   
 }
